d9379b45c5a5fdce5dd74e1cdc0c1aa44c6af824
JamanRased <jaman.rashed.199@gmail.com>
2021-12-12 22:49:52 +0600

    router setup done

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+###EmajonmainshopData
+https://raw.githubusercontent.com/ProgrammingHero1/ema-john-simple-resources/master/fakeData/products.JSON
+###Shop
+https://ema-john.firebaseapp.com/
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
+      <Router>
       <Header></Header>
-      <Shop></Shop>
+      <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/orderreview">
+           <OrderReview></OrderReview>
+          </Route>
+          <Route path="/orders">
+           <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+           <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+           <NotFound></NotFound>
+          </Route>
+        </Switch>
+      
+      
+      </Router>
     </div>
   );
 }
